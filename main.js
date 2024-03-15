8e38e3bc07a968c474ecd6334bd0584a35ab88b2
Brenda Orozco <orozcobrenda231@gmail.com>
2024-03-14 21:02:52 -0300

    modificaciones en el codigo

--- main.js
+++ main.js
@@ -1,179 +1,6 @@
-/* - PERFUMERÍA CANDYFLIP -*/
-
-/*function consultarPrecio(codigo) {
-    switch(codigo) {
-        case 1:
-            alert("El valor del Gel de limpieza es de $2890")
-            break
-        case 2:
-            alert("El valor del Serum H es de $8350")
-            break
-        case 3:
-            alert("El valor de la crema hidratante es de $5200")
-            break
-        case 4:
-            alert("El valor de la crema humectante es de $4900")
-            break
-        case 5: 
-            alert("El valor del protector solar es de $10500")
-            break
-        default:
-            alert("Producto no disponible")
-    }
-}
-
-function consultarStock(codigo) {
-    switch(codigo) {
-        case 1:
-            alert("El producto se encuentra disponible")
-            break
-        case 2:
-            alert("El producto se encuentra disponible")
-            break
-        case 3:
-            alert("El producto se encuentra disponible")
-            break
-        case 4:
-            alert("El producto se encuentra disponible")
-            break
-        case 5: 
-            alert("El producto se encuentra disponible")
-            break
-        default:
-            alert("Producto no disponible")
-    }
-}
-
-function mostrarCuotas() {
-    console.log("Puede abonar en:")
-    for ( let i = 1; i <= 3; i++) {
-        console.log(i + " Cuota(s) sin interés.")
-    }
-}
 
 
-function consultarProducto() {
-    let producto = prompt("Ingrese el código del producto");
 
-    if (producto !== null && producto !== "") {
-        consultarStock(parseInt(producto));
-        consultarPrecio(parseInt(producto));
-
-        let realizarCompra = confirm("¿Desea realizar la compra con tarjeta?");
-        
-        if (realizarCompra) {
-            mostrarCuotas();
-        }
-    } else {
-        console.warn("Producto no disponible");
-    }
-}*/
-
-
-/* SEGUNDA PRE-ENTREGA */
-/* CARRITO DE COMPRAS DE "PERFUMERIA CANDYFLIP" */
-
-// const carrito =[]
-
-// const productos = [{codigo: 1, nombre: 'Gel de limpieza', precio: 8999},
-//                      {codigo: 2, nombre:'Serum H', precio: 7550},
-//                      {codigo: 3, nombre:'Crema hidratante', precio: 4950}, 
-//                      {codigo: 4, nombre:'Crema humectante', precio: 4950},
-//                      {codigo: 5, nombre:'Protector solar' , precio: 12500},
-//                      {codigo: 6, nombre:'Agua termal' , precio: 10500}]
-
-// function buscarProducto(codigo) {
-//      let productoSeleccionado = productos.find((producto)=> producto.codigo === parseInt(codigo))
-//      return productoSeleccionado
-// }
-
-// function comprar() {
-//      let codigoProducto = prompt("Ingrese el código del producto:");
-
-//      let productoElegido = buscarProducto(codigoProducto);
-
-//      if (productoElegido === undefined) {         alert("Error. Código no encontrado.");
-//      } else {
-//          let confirmacion = confirm("El producto " + productoElegido.nombre + " está seleccionado. ¿Desea agregarlo al carrito?");
-        
-//          if (confirmacion) {
-//              carrito.push(productoElegido);
-//              alert(productoElegido.nombre + " ha sido agregado al carrito.");
-//          } else {
-//              alert("Producto no agregado al carrito.");
-//          }
-
-//          let respuesta = confirm("¿Desea buscar otro producto?");
-
-//          if (respuesta) {
-//              comprar();
-//          } else {             console.clear();
-//              console.table(carrito);
-//              const shopping = new Compra(carrito);
-//              console.log("El total de tu compra es de: $ " + shopping.obtenerSubtotal());
-//              console.log("¡Muchas gracias por tu compra! ♥");
-//          }
-//      }
-// }
-/* TERCER PRE-ENTREGA */
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const botonesComprar = document.querySelectorAll(".btn-primary");
-
-//     botonesComprar.forEach((boton) => {
-//         boton.addEventListener("click", function (event) {
-//             event.preventDefault();
-//             const codigoProducto = boton.dataset.codigo;
-//             const productoElegido = buscarProducto(codigoProducto);
-
-//             if (productoElegido === undefined) {
-//                 mostrarMensaje("Error. Código no encontrado.");
-//             } else {
-//                 mostrarConfirmacion(`¿Agregar ${productoElegido.nombre} al carrito?`, () => {
-//                     carrito.push(productoElegido);
-//                     mostrarMensaje(`${productoElegido.nombre} ha sido agregado al carrito.`);
-//                 });
-//             }
-//         });
-//     });
-
-//     function mostrarMensaje(mensaje) {
-//         const mensajeElement = document.createElement("div");
-//         mensajeElement.classList.add("mensaje");
-//         mensajeElement.textContent = mensaje;
-//         document.body.appendChild(mensajeElement);
-
-//         setTimeout(() => {
-//             mensajeElement.remove();
-//         }, 3000);
-//     }
-
-//     function mostrarConfirmacion(mensaje, callback) {
-//         const confirmacion = document.createElement("div");
-//         confirmacion.classList.add("confirmacion");
-//         confirmacion.innerHTML = `
-//             <p>${mensaje}</p>
-//             <button class="btn-confirmar">Confirmar</button>
-//             <button class="btn-cancelar">Cancelar</button>
-//         `;
-//         document.body.appendChild(confirmacion);
-
-//         const btnConfirmar = confirmacion.querySelector(".btn-confirmar");
-//         const btnCancelar = confirmacion.querySelector(".btn-cancelar");
-
-//         btnConfirmar.addEventListener("click", () => {
-//             confirmacion.remove();
-//             callback();
-//         });
-
-//         btnCancelar.addEventListener("click", () => {
-//             confirmacion.remove();
-//         });
-//     }
-// });
-
-
-/* PROYECYO FINAL */
 
 document.addEventListener("DOMContentLoaded", function() {
     const searchInput = document.getElementById('searchInput');
@@ -185,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let carrito = [];
 
     const productos = [
-        { nombre: 'Gel de limpieza', descripcion: 'Gel de limpieza apto para todo tipo de piel.', precio: 8999 },
+        { nombre: 'Gel de limpieza', descripcion: 'Gel de limpieza apto para todo tipo de piel.', precio: 8999, imagen: ".\multimedia\gel_aveno1.jpg" },
         { nombre: 'Serum H', descripcion: 'Serum ultra hidratante concentrado.', precio: 12500 },
         { nombre: 'Crema hidratante', descripcion: 'Crema hidratante Allday', precio: 11000},
         { nombre: 'Crema humectante', descripcion: 'Crema humectante con proteccion UV', precio: 11000 },
@@ -215,13 +42,12 @@ document.addEventListener("DOMContentLoaded", function() {
             console.error("No se pudo encontrar el contenedor de tarjetas.");
             return;
         }
-    
         cardsContainer.innerHTML = '';
         productos.forEach(producto => {
             const card = `
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="https://via.placeholder.com/150" class="card-img-top" alt="Product Image">
+                        <img src="${producto.imagen}" class="card-img-top" alt="Product Image">
                         <div class="card-body">
                             <h5 class="card-title">${producto.nombre}</h5>
                             <p class="card-text">${producto.descripcion}</p>
@@ -264,7 +90,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const carritoGuardado = JSON.parse(localStorage.getItem("carrito")) || [];
         carritoGuardado.forEach(producto => {
             const li = document.createElement("li");
-            li.textContent = `${producto.nombre} - Precio: $${producto.precio}`; // Agregar precio
+            li.textContent = `${producto.nombre} - Precio: $${producto.precio}`;
             cartList.appendChild(li);
         });
     }
@@ -272,7 +98,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.addEventListener("DOMContentLoaded", function() {
         const searchInput = document.getElementById('searchInput');
-        // Tu código que utiliza el searchInput
+
         searchInput.addEventListener('input', function(event) {
             const termino = event.target.value;
             buscarProductos(termino);
@@ -287,7 +113,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (event.target.classList.contains("addToCartBtn")) {
             event.preventDefault();
             const productoNombre = event.target.parentElement.querySelector(".card-title").textContent;
-            const producto = productos.find(p => p.nombre === productoNombre); // Buscar el producto por nombre
+            const producto = productos.find(p => p.nombre === productoNombre);
             addToCart(producto);
         }
     });
